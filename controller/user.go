996e10834022f0fd5ab2ec9ae89a8c7091e02c2b
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-12-18 20:57:01 +0700

    add func

--- controller/user.go
+++ controller/user.go
@@ -1,6 +1,9 @@
 package controller
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func Register(respw http.ResponseWriter, req *http.Request) {
 	
@@ -10,3 +13,10 @@ func Login(respw http.ResponseWriter, req *http.Request) {
 	
 }
 
+func sendErrorResponse(w http.ResponseWriter, message string, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": message,
+	})
+}
