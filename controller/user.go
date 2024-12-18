439762fe66f30135b968830bd3ad4122eed17d89
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-12-18 20:26:22 +0700

    change func name to Register and Login

--- controller/user.go
+++ controller/user.go
@@ -2,6 +2,11 @@ package controller
 
 import "net/http"
 
-func PostUser(respw http.ResponseWriter, req *http.Request) {
+func Register(respw http.ResponseWriter, req *http.Request) {
 	
 }
+
+func Login(respw http.ResponseWriter, req *http.Request) {
+	
+}
+
